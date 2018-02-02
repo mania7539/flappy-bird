18f75680934478d87da5892342e2885f9a627c92
David.Rz.Liu <mania7539@gmail.com>
2018-02-03 00:13:43 +0800

    Refactor: in math part, make FloatBuffer, add comments and integrate codes

--- src/tw/ray/math/Matrix4f.java
+++ src/tw/ray/math/Matrix4f.java
@@ -2,63 +2,78 @@ package tw.ray.math;
 
 import static java.lang.Math.*;
 
+import java.nio.FloatBuffer;
+
+import tw.ray.util.BufferUtils;
+
 public class Matrix4f {
-    public float[] matrix = new float[4 * 4];
+    public final static int SIZE = 4 * 4;
+    public float[] elements = new float[SIZE];
+
+    public FloatBuffer toFloatBuffer() {
+        return BufferUtils.createFloatBuffer(elements);
+    }
     
     /**
-     * Create an identity matrix, in which all elements in the main diagonal
-     *  are equal to 1, and everything else is 0. Everything * Identity will still be itself. 
+     * Create an identity matrix, in which all elements in the main diagonal are
+     * equal to 1, and everything else is 0. Everything * Identity will still be
+     * itself.
+     * 
      * @return
      */
     public static Matrix4f identify() {
         Matrix4f result = new Matrix4f();
-        
-        for (int i=0; i< 4 * 4; i++) {
-            result.matrix[i] = 0.0f;
+
+        for (int i = 0; i < SIZE; i++) {
+            result.elements[i] = 0.0f;
         }
         
-        result.matrix[0 * 4 + 0] = 1.0f;
-        result.matrix[1 * 4 + 1] = 1.0f;
-        result.matrix[2 * 4 + 2] = 1.0f;
-        result.matrix[3 * 4 + 3] = 1.0f;
-        
+        // Take notes:  row , col: since OpenGL is a column major ordering system
+        result.elements[0 + 0 * 4] = 1.0f;
+        result.elements[1 + 1 * 4] = 1.0f;
+        result.elements[2 + 2 * 4] = 1.0f;
+        result.elements[3 + 3 * 4] = 1.0f;
+
         return result;
     }
-    
+
     public Matrix4f multiply(Matrix4f matrix) {
         Matrix4f result = new Matrix4f();
-        
-        for (int x=0; x<4; x++) {
-            for (int y=0; y<4; y++) {
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
                 float sum = 0;
-                for (int e=0; e<4; e++) {
-                    sum += this.matrix[x*4 + e] * matrix.matrix[e*4 + y];
+                for (int e = 0; e < 4; e++) {
+                    sum += this.elements[x * 4 + e] * matrix.elements[e * 4 + y];
                 }
-                result.matrix[x*4 + y] = sum;
+                result.elements[x * 4 + y] = sum;
             }
         }
-        
+
         return result;
     }
-    
-    /** 
-     * Move an object around 2D or 3D space,
-     *  OpenGL takes column major ordering for matrices
+
+    /**
+     * Move an object around 2D or 3D space, OpenGL takes column major ordering
+     * for matrices
+     * 
      * @param vector
      * @return
      */
     public static Matrix4f translate(Vector3f vector) {
         Matrix4f result = identify();
         
-        result.matrix[3 * 4 + 0] = vector.x;
-        result.matrix[3 * 4 + 1] = vector.x;
-        result.matrix[3 * 4 + 2] = vector.x;
-        
+        // Take notes:  row , col: since OpenGL is a column major ordering system
+        result.elements[0 + 3 * 4] = vector.x;
+        result.elements[1 + 3 * 4] = vector.x;
+        result.elements[2 + 3 * 4] = vector.x;
+
         return result;
     }
-    
+
     /**
      * Rotate along the z-axis
+     * 
      * @param angle
      * @return
      */
@@ -68,17 +83,19 @@ public class Matrix4f {
         float cos = (float) cos(r);
         float sin = (float) sin(r);
         
-        result.matrix[0 * 4 + 0] = cos;
-        result.matrix[0 * 4 + 1] = sin;
-        
-        result.matrix[0 * 4 + 2] = -sin;
-        result.matrix[0 * 4 + 3] = cos;
-        
+        // Take notes:  row , col: since OpenGL is a column major ordering system
+        result.elements[0 + 0 * 4] = cos;
+        result.elements[1 + 0 * 4] = sin;
+
+        result.elements[2 + 0 * 4] = -sin;
+        result.elements[3 + 0 * 4] = cos;
+
         return result;
     }
-    
+
     /**
      * Rotate around all axis
+     * 
      * @param angle
      * @param x
      * @param y
@@ -92,23 +109,25 @@ public class Matrix4f {
         float sin = (float) sin(r);
         float omc = 1.0f - cos;
         
-        result.matrix[0 * 4 + 0] = x * omc + cos;
-        result.matrix[0 * 4 + 1] = x * y * omc + z * sin;
-        result.matrix[0 * 4 + 2] = x * z * omc - y * sin;
-        
-        result.matrix[1 * 4 + 0] = x * y * omc - z * sin;
-        result.matrix[1 * 4 + 1] = y * omc + cos;
-        result.matrix[1 * 4 + 2] = y * z * omc + x * sin;
-        
-        result.matrix[2 * 4 + 0] = x * z * omc + y * sin;
-        result.matrix[2 * 4 + 1] = y * z * omc - x * sin;
-        result.matrix[2 * 4 + 2] = z * omc + cos;
-        
+        // Take notes:  row , col: since OpenGL is a column major ordering system
+        result.elements[0 + 0 * 4] = x * omc + cos;
+        result.elements[1 + 0 * 4] = x * y * omc + z * sin;
+        result.elements[2 + 0 * 4] = x * z * omc - y * sin;
+
+        result.elements[0 + 1 * 4] = x * y * omc - z * sin;
+        result.elements[1 + 1 * 4] = y * omc + cos;
+        result.elements[2 + 1 * 4] = y * z * omc + x * sin;
+
+        result.elements[0 + 2 * 4] = x * z * omc + y * sin;
+        result.elements[1 + 2 * 4] = y * z * omc - x * sin;
+        result.elements[2 + 2 * 4] = z * omc + cos;
+
         return result;
     }
-    
+
     /**
      * Projection
+     * 
      * @param left
      * @param right
      * @param bottom
@@ -120,14 +139,15 @@ public class Matrix4f {
     public static Matrix4f orthographic(float left, float right, float bottom, float top, float near, float far) {
         Matrix4f result = identify();
         
-        result.matrix[0 * 4 + 0] = 2.0f / (right - left);
-        result.matrix[1 * 4 + 1] = 2.0f / (top - bottom);
-        result.matrix[2 * 4 + 2] = 2.0f / (near - far);
-        
-        result.matrix[3 * 4 + 0] = (left - right) / (left - right);
-        result.matrix[3 * 4 + 1] = (bottom + top) / (bottom - top);
-        result.matrix[3 * 4 + 2] = (near + far) / (far - near);
-        
+        // Take notes:  row , col: since OpenGL is a column major ordering system
+        result.elements[0 + 0 * 4] = 2.0f / (right - left);
+        result.elements[1 + 1 * 4] = 2.0f / (top - bottom);
+        result.elements[2 + 2 * 4] = 2.0f / (near - far);
+
+        result.elements[0 + 3 * 4] = (left - right) / (left - right);
+        result.elements[1 + 3 * 4] = (bottom + top) / (bottom - top);
+        result.elements[2 + 3 * 4] = (near + far) / (far - near);
+
         return result;
     }
 }
