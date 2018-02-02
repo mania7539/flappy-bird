d9ba04b0c24e849dbcc3a6255bedefff3ff0bbc2
David.Rz.Liu <mania7539@gmail.com>
2018-02-02 23:15:37 +0800

    Ep. 9: LWJGL 3 - replace code with glfw window framework

--- src/tw/ray/Main.java
+++ src/tw/ray/Main.java
@@ -1,13 +1,12 @@
 package tw.ray;
 
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 
 import tw.ray.graphics.Shader;
 import tw.ray.math.Vector3f;
@@ -19,59 +18,68 @@ public class Main implements Runnable {
     private String title = "Flappy Bird";
     private boolean running = false;
     private Thread thread;
-    
+    private long window;
+
     public void start() {
         running = true;
         thread = new Thread(this, "Display");
         thread.start();
     }
-    
+
     private void init() {
         String version = glGetString(GL_VERSION);
         System.out.println(String.format("OpenGL %s", version));
-        
+
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     }
-    
-    public void render() {
+
+    private void update() {
+        glfwPollEvents();   // deal with key events
+    }
+
+    private void render() {
+        glfwSwapBuffers(window);
         glClear(GL_COLOR_BUFFER_BIT);
         glDrawArrays(GL_TRIANGLES, 0, 3);
     }
-    
+
     public void run() {
-        try {
-            Display.setDisplayMode(new DisplayMode(width, height));
-            Display.setTitle(title);
-            ContextAttribs context = new ContextAttribs(3, 3);
-            
-            if (System.getProperty("os.name").contains("Mac")) {
-                context = new ContextAttribs(3, 2);
-            }
-            
-            Display.create(new PixelFormat(), context.withProfileCore(true));
-            
-        } catch (LWJGLException e) {
-            e.printStackTrace();
+        if (glfwInit() != GL_TRUE) {
+            return;
         }
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        window = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (window == NULL) {
+            return;
+        }
+
+        GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(window, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+        GL.createCapabilities();
         
         init();
-        
+
         int vao = glGenVertexArrays();
         glBindVertexArray(vao);
-        
+
         Shader shader = Shader.BASIC;
         shader.enable();
         shader.setUniform3f("col", new Vector3f(0.8f, 0.2f, 0.3f));
 
         while (running) {
+            update();
             render();
-            Display.update();
-            if (Display.isCloseRequested()) running = false;
+            
+            if (glfwWindowShouldClose(window) == GL_TRUE) {
+                running = false;
+            }
         }
-        Display.destroy();
     }
-    
+
     public static void main(String[] args) {
         new Main().start();
     }
